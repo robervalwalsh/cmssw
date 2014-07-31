dc7cead4dbefda62233597cd67d5539d02ecc7ed
Roberval Walsh <roberval.walsh@gmail.com>
2014-07-31 16:58:47 +0200

    Cleaner approach. Transfer of analyzer to a library

--- BrilAnalysis/Bcm1f/analysis/simrates.py
+++ BrilAnalysis/Bcm1f/analysis/simrates.py
@@ -14,10 +14,9 @@ sys.path.append( "lib" )
 from utils import MyOptionParser
 from utils import TextColor
 from utils import get_list_of_files
-from data_model import MyPSimHit
+from analyzers import EdmSimAnalyzer
 
 import ROOT
-from DataFormats.FWLite import Events, Handle
 
 ROOT.gROOT.SetBatch()
 ROOT.gROOT.SetStyle('Plain') # white background
@@ -37,29 +36,13 @@ def main():
    # get list of files
    files_list = get_list_of_files( options.input )
    nevents = options.nevents
+   pileup = options.pileup
    
-   event_counter = 0
-#      ''' https://cmssdt.cern.ch/SDT/doxygen/CMSSW_5_3_9/doc/html/d4/dab/classpython_1_1Events.html
-#This is NOT a collection of fwlite::Event objects.
-#The fwlite::Event object is obtained using the object() method (see below) '''
-   events = Events(files_list)
-   handle = Handle ('std::vector<PSimHit>')
-   label = ('g4SimHits','BCM1FHits','SIM')
+   analysis = EdmSimAnalyzer(files_list,pileup)
+   analysis.analyze()
    
-   # Create histograms, etc.
-   tof_hist = ROOT.TH1F ("tof", "time of flight", 100, 0, 100)   
-   # loop over events
-   for event in events:
-      # use getByLabel, just like in cmsRun
-      event.getByLabel (label, handle)
-      psimhits = handle.product()
-      nhits = len(psimhits)
-      for i in xrange(nhits):
-         myhit = MyPSimHit ()
-         myhit.set_time_of_flight(psimhits[i].timeOfFlight())
-         tof_hist.Fill(myhit.time_of_flight())
-      event_counter += 1
-          
+   tof_hist = analysis.histograms()[0]
+   
    c1 = ROOT.TCanvas()
    tof_hist.Draw()
    c1.Print ("tof_hist.png")
