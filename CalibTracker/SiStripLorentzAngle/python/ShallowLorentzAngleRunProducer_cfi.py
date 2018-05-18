edd0b5002319e7ff7fe474358128b9f522d84be1
Roberval Walsh <roberval.walsh@gmail.com>
2018-05-18 18:49:11 +0200

    remove temptive plugin to save LA run info

--- CalibTracker/SiStripLorentzAngle/python/ShallowLorentzAngleRunProducer_cfi.py
+++
@@ -1,5 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-
-shallowLorentzAngleRunProducer = cms.EDProducer("ShallowLorentzAngleRunProducer",
-                                      Prefix=cms.string(""),
-                                      Suffix=cms.string(""))
